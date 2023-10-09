eba397c7ff1c921024a787c0a49c0d376eb88014
Hayk <haykghazakhyan@gmail.com>
2023-10-09 21:14:23 +0400

    commited all changes

--- Navin/NavinProject/NavinProject/settings.py
+++ Navin/NavinProject/NavinProject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travello.apps.TravelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'NavinProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'telusko',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost'
+
     }
 }
 
@@ -121,6 +126,10 @@ STATICFILES_DIRS = [
      os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- Navin/NavinProject/travello/models.py
+++ Navin/NavinProject/travello/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Destination:
-    price: int
-    name: str
-    img: str
-    desc: str
-    offer: bool
+class Destination(models.Model):
+    
+
+    price = models.IntegerField()
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField()
+    offer = models.BooleanField(default=False)
+
